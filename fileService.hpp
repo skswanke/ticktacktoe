ec896f6388d00069f59a7c728998321f6519dbac
skswanke <skswanke@users.noreply.github.com>
2016-12-01 13:28:34 -0500

    Update fileService.hpp

--- fileService.hpp
+++ fileService.hpp
@@ -1,1 +1,18 @@
+// Sam Swanke
 
+#ifndef fileService_hpp
+#define fileService_hpp
+#include <string>
+#include <iostream>
+
+class FileService {
+public:
+    // Only default constructor for new game
+    FileService();
+    // Starts the game.
+    // R: n/a, M: n/a, E: n/a
+    void save(string s);
+    string load();
+};
+
+#endif
