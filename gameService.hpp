b55e44e0c62336832890e1bc6839b27d4acfe3ad
skswanke <skswanke@users.noreply.github.com>
2016-12-01 13:26:37 -0500

    Update gameService.hpp

--- gameService.hpp
+++ gameService.hpp
@@ -1,1 +1,19 @@
+// Sam Swanke
 
+#ifndef gameService_hpp
+#define gameService_hpp
+#include <string>
+
+class GameService {
+public:
+    // Only default constructor for new game
+    GameService();
+    // Checks if the game is won or lost.
+    // R: n/a, M: n/a, E: n/a
+    int isOver(string s);
+    // Displays end logic.
+    // R: n/a, M: n/a, E: n/a
+    void end(int i);
+};
+
+#endif
